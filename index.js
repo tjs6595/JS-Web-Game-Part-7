c8751013267c94f2486f4efbe5dc12e320900862
tjs6595 <t.schultejans@schenckprocess.com>
2022-04-27 23:40:10 -0500

    This is my Java Script Web Game Part 7 Submission with an attempt at the bonus that didn't work.

--- index.js
+++ index.js
@@ -4,8 +4,23 @@ const pc = newPlayableCharacter(100, 110)
 // Create a non-playable character
 const npc = newNonPlayableCharacter(50, 300)
 
-// have the NPC start walking east immediately
-npc.walkEast()
+//let looptrack = true;
+
+//while(looptrack = true){
+    async function moveNPC(){
+        // have the NPC start walking east immediately
+        await npc.walkNorth(1400)
+        await npc.walkEast(1200)
+        await npc.walkSouth(300)
+        await npc.walkEast(1500)
+        await npc.walkSouth(1500)
+        await npc.walkWest(2700)
+        await npc.walkNorth(400)
+    }
+//}
+
+moveNPC();
+
 
 // Create the inventory
 const inventory = newInventory()
